bddf397eda9918bb49626ec78f5a9bbfa40497c2
ICX\aliaksandr.nikifarau <alexandernanotexno@gmail.com>
2017-11-24 16:24:12 +0300

    'ready'

--- script.js
+++ script.js
@@ -45,26 +45,77 @@ var way;
                 let i = __me.headerRow.indexOf(value[0]),
                     j = __me.headerRow.indexOf(value[1]),
                     n = value[2] / value[3];
-                __me.matrix[i][j] = n;
+                __me.matrix[i][j] = { value: n, visitedAmount: 0 };
             });
         }
 
         findWay() {
             let str = '',
-            price = 0;
-            for (let i = this.start.index; i < this.matrix.length; i++) {
-                outer: for (let j = 0; j < this.matrix[i].length; j++) {
-                    if (this.matrix[i][j] != undefined && this.matrix[i][j] != 0) {
-                        str += this.headerRow[j] + ' > ';
-                        price += this.matrix[i][j];
-                        this.matrix[i][j] = 0;
-                        i = j - 1;
-                        break outer;
+                price = 0,
+                arr = [],
+                lastVisitedAmount = 9999999;
+            for (let k = this.start.index; k < this.matrix.length; k++) {
+                if (this.matrix[this.start.index][k] != undefined) {
+                    price = this.matrix[this.start.index][k].value;
+                    for (let l = k, q = k; l < this.matrix.length; l++) {
+                        newRoute: for (let i = q; i < this.matrix[k].length; i++) {
+                            for (let j = 0; j < this.matrix[k].length; j++) {
+                                if (this.matrix[i][j] != undefined) {
+                                    if (!this.isRowVisited(this.matrix[i]) && this.matrix[i][j].visitedAmount != this.getMinVisitedAmount(this.matrix[i])) {
+                                        continue;
+                                    }
+                                    let qweasd = this.matrix[i][j];
+                                    str += this.headerRow[i] + ' > ';
+                                    price += this.matrix[i][j].value;
+                                    this.matrix[i][j].visitedAmount += 1;
+                                    i = j - 1;
+                                    if(j = this.end.index){
+                                        break;
+                                    }
+                                    break;
+                                }
+                            }
+                        }
+
+                        arr.push({ route: `A > ${str}F`, price: price });
+                        str = '';
+                        price = this.matrix[this.start.index][k].value;
                     }
                 }
             }
-            console.log(str);
-            console.log(price);            
+            console.log(arr);
+        }
+
+        isRowEmpty(row) {
+            row.forEach(function () {
+                if (row != undefined) {
+                    return false;
+                }
+                return true;
+            });
+        }
+        isRowVisited(row) {
+            let min = 9999999999;
+            row.forEach(function (item) {
+                if (item != undefined) {
+                    min = (min > item.visitedAmount) ? item.visitedAmount : min;
+                }
+            });
+            for (let i = 0; i < row.length; i++) {
+                if (row[i] != undefined && row[i].visitedAmount != min) {
+                    return false;
+                }
+            }
+            return true;
+        }
+        getMinVisitedAmount(row){
+            let min = 9999999999;
+            row.forEach(function (item) {
+                if (item != undefined) {
+                    min = (min > item.visitedAmount) ? item.visitedAmount : min;
+                }
+            });
+            return min;
         }
     }
 
@@ -72,9 +123,12 @@ var way;
     ['A', 'C', 120, 60],
     ['B', 'D', 210, 21],
     ['B', 'C', 100, 20],
+    ['B', 'E', 300, 50],
     ['C', 'E', 300, 100],
     ['E', 'D', 200, 50],
-    ['D', 'F', 110, 10]];
+    ['E', 'F', 300, 100],
+    ['D', 'F', 110, 10],
+];
 
     way = new Way({ data: data, a: 'A', b: 'F' });
 
